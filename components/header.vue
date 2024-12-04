f298d01dce580484f8b46cf47d63db45c5a86741
ginko lee <492180240@qq.com>
2024-12-04 16:07:11 +0800

    feat: :sparkles: add tailwindcss then set a custom font-family

---
+++ components/header.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>header.vue</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  modules: ["@nuxtjs/tailwindcss"],
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: { primary: "rgb(45, 47, 49)" },
+          fontFamily: {
+            sans: ["Clone Udemy Text", ...defaultTheme.fontFamily.sans],
+          },
+        },
+      },
+    },
+  },
+});
